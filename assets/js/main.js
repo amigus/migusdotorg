70da45f15b2c22e666e35d97e86dd60083f08ec9
Adam Migus <adam@migus.org>
2024-05-24 10:43:06 -0400

    Exclude public.asc as a possible redirect

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,8 @@ function randomRedirect(timeout, checkboxId) {
     setTimeout(function() {
         var checkbox = document.getElementById(checkboxId);
         if (!checkbox.checked) {
-            var links = document.body.getElementsByTagName('a');
+            var links = Array.from(document.body.getElementsByTagName('a'));
+            links = links.filter(link => link.href !== '/public.asc');
             if (links.length > 0) {
                 var randomLink = links[Math.floor(Math.random() * links.length)];
                 window.location.href = randomLink.href;
@@ -10,5 +11,4 @@ function randomRedirect(timeout, checkboxId) {
         }
     }, timeout);
 }
-
 randomRedirect(60000, 'redirect_cb');
