c13945b74a17447f952656976e17bdb07cafbed3
Adam Migus <adam@migus.org>
2024-12-01 15:34:25 -0500

    Remove Google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     content: ["./layouts/**/*.{html,js}"],
     theme: {
       container: {
+        center: true,
         padding: {
           DEFAULT: '1rem',
           sm: '2rem',
@@ -10,10 +11,6 @@ module.exports = {
           xl: '4rem',
           '2xl': '5rem',
         },
-      },
-      fontFamily: {
-        monospace: ['Fira Mono', 'monospace'],
-        sans: ['Lato', 'sans-serif'],
       },
       screens: {
         sm: '480px',
